7217547eaf0c79eb7f992ce0f64bb400d25af64f
Matteo Di Carlo <matteo.dicarlo93@gmail.com>
2020-07-09 11:35:15 +0200

    calculation of DeltaZ

--- bin/functions.cpp
+++ bin/functions.cpp
@@ -9,31 +9,57 @@
 #include <fstream>
 #include <sstream>
 #include "global.hpp"
+#include "aliases.hpp"
 
 using namespace Eigen;
 using namespace std;
 
 
 
-// vector<double> compute_V(vd_t Int)
-// {
-//   vector<double> V(5);
-//
-//   double h1[5] = {  4.0, -4.0, -2.0,  2.0, 0.0};
-//   double h2[5] = { 16.0, 16.0,  4.0,  4.0, 0.0};
-//   double h3[5] = {  1.0, -1.0,  0.5, -0.5, 0.0};
-//   double h4[5];
-//   for (int i=0; i<5, i++){
-//     h4[i] = (h2[i]/4.0-1.0)/3.0;
-//
-//
-//     V[i] = VV00 + VV10 + csw*VV11 + csw*csw*VV12
-//   }
-//
-//
-//   return V;
-// }
-// double compute_S(vd_t Int)
-// {
-//
-// }
+vector<double> compute_V(vd_t& Int)
+{
+  vector<double> V(5);
+  double VV00, VV10, VV11, VV12;
+  double csw2 = csw*csw;
+
+  double h1[5] = {  4.0, -4.0, -2.0,  2.0, 0.0};
+  double h2[5] = { 16.0, 16.0,  4.0,  4.0, 0.0};
+  double h3[5] = {  1.0, -1.0,  0.5, -0.5, 0.0};
+  double h4[5];
+  for (int i=0; i<5; i++){
+    h4[i] = (h2[i]/4.0-1.0)/3.0;
+
+    VV00 = (1.0+(2.0+alpha)*h4[i])*Int[0] + (alpha+3*h4[i])*Int[1];
+    VV10 = Int[2]+alpha*Int[3]+(Int[4]+alpha*Int[5])*h1[i] + (Int[6]+alpha*Int[7])*h4[i];
+    VV11 = Int[8]*(h1[i]+4.0*h4[i]);
+    VV12 = Int[9]*h1[i] + Int[10]*h3[i] + Int[11]*h4[i];
+
+    V[i] = VV00 + VV10 + csw*VV11 + csw2*VV12;
+  }
+
+
+  return V;
+}
+double compute_S(vd_t& Int)
+{
+  double S = Int[0] + alpha*Int[1] + Int[2] + alpha*Int[3] + csw*Int[4] + csw*csw*Int[5];
+
+  return S ;
+}
+vector<double> compute_Z(vd_t& Int, vd_t& IntS)
+{
+  vector<double> Z(6);
+
+  vector<double> V=compute_V(Int);
+  double S=compute_S(IntS);
+
+  double g02 = 6.0/beta;
+  double as = g02/(16.0*M_PI*M_PI)*4.0/3.0;
+
+  Z[0] = -S*as;
+  for(int i=0; i<5; i++)
+  {
+    Z[i+1] = Z[0] - V[i]*as;
+  }
+  return Z;
+}
--- bin/functions.hpp
+++ bin/functions.hpp
@@ -83,18 +83,10 @@ inline Matrix4d make_propagator(Vector4d kt2_dir, Vector4d kt4_dir, Vector4d kt6
   return A;
 }
 
-// double prod(Vector4d a);
-// double prod(Vector4d a,Vector4d b);
-// double prod(Vector4d a,Vector4d b,Vector4d c);
-// double prod(Vector4d a,Vector4d b,Vector4d c,Vector4d d);
-// double prod(Vector4d a,Vector4d b,Vector4d c,Vector4d d,Vector4d e);
-//
-// Vector4d cwiseProduct(Vector4d a, Vector4d b);
-// Vector4d cwiseProduct(Vector4d a, Vector4d b, Vector4d c);
-// Vector4d cwiseProduct(Vector4d a, Vector4d b, Vector4d c, Vector4d d);
-// Vector4d cwiseProduct(Vector4d a, Vector4d b, Vector4d c, Vector4d d, Vector4d e);
-//
-//
-// Matrix4d make_propagator(Vector4d kt2_dir, Vector4d kt4_dir, Vector4d kt6_dir);
+vector<double> compute_Z(valarray<double>& Int,valarray<double>& IntS);
+
+// vector<double> compute_V(valarray<double>& Int);
+// double compute_S(valarray<double>& Int);
+
 
 #endif
